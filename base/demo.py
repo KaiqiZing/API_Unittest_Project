3254e9bb282cd8bef4b605b9ad74df8ca6d169d8
KaiqiZing <zhang20710730@gmail.com>
2022-05-06 18:22:27 +0800

    unittest和requests的使用

---
+++ base/demo.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+import requests
+import json
+"""
+request的使用规则
+"""
+class RunMain:
+    def __init__(self,url,method,data=None):
+        self.res = self.run_main(url, method, data)
+
+    def send_get(self, url, data):
+        res = requests.get(url=url, data=data).json()
+        return res
+
+    def send_post(self, url, data):
+        res = requests.post(url=url, data=data).json()
+        return res
+
+    def run_main(self,url, method, data=None):
+        res = None
+        if method == "GET":
+            res = self.send_get(url, data)
+        else:
+            res = self.send_post(url, data)
+
+        return res
+
+if __name__ == '__main__':
+    url = 'http://sellshop.5istudy.online/sell/buyer/product/list'
+    run = RunMain(url,'GET')
+    print(run.res)
---
+++ base/test_method.py
@@ -0,0 +1,53 @@
+#coding=utf-8
+# import HTMLTestRunner
+import unittest
+import json
+from demo import RunMain
+
+
+class TestMethod(unittest.TestCase):
+
+    # 测试前置和后置
+    def setUp(self):
+        print("这是前置执行")
+    def test_01(self):
+        url = 'http://sellshop.5istudy.online/sell/buyer/product/list'  # 正确的地址
+
+        # url = 'http://sellshop.5istudy.online/sell/buyer/product/liszt'  # 错误的地址
+        self.run = RunMain(url,"GET")
+        res = self.run.run_main(url, "GET")
+
+        # 引入断言做判断
+        self.assertNotEqual(res['message'], "No message available", "测试失败")
+        print("这是第一个case")
+
+    # @unittest.skip("test_02")  # 指定跳过该用例执行
+    def test_02(self):
+        url = 'http://coding.imooc.com/api/cate'
+        data = {
+            'timestamp': '1507034803124',
+            'uid': '5249191',
+            'uuid': '5ae7d1a22c82fb89c78f603420870ad7',
+            'secrect': '078474b41dd37ddd5efeb04aa591ec12',
+            'token': '7d6f14f21ec96d755de41e6c076758dd',
+            'cid': '0'
+        }
+
+        self.run = RunMain(url,"POST",data)
+        res = self.run.run_main(url, "POST", data)
+
+        # 引入断言做判断
+        # Python源代码错误，双重否定就是肯定
+        self.assertNotEqual(res['errorCode'],1007,"测试失败")
+        print("这是第二个case")
+
+
+if __name__ == '__main__':
+    # 第一种执行方式：
+    # unittest.main()
+
+    #第二种执行方式---问题执行时不能单独执行
+    suite = unittest.TestSuite()
+    suite.addTest(TestMethod("test_02"))
+    result = unittest.TestResult()
+    suite.run(result)
